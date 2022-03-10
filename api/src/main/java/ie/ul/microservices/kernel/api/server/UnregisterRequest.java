858a9a3e410718cfffaed35d3ed3ee2f39b5142d
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2022-03-10 14:57:21 +0000

    Registration Controller register method
    Fix for getMicroservice(name) in registry

--- api/src/main/java/ie/ul/microservices/kernel/api/server/UnregisterRequest.java
+++
@@ -1,9 +0,0 @@
-package ie.ul.microservices.kernel.api.server;
-
-/**
- * This class represents a request to unregister a microservice from the kernel
- * TODO think up fields
- */
-public class UnregisterRequest {
-    //ref to microservice that wants to eunRgister?
-}
