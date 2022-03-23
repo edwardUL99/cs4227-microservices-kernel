8f3e96c18c22b29608aa2ab21a77ebd4e5dc6e8f
Edward Lynch-Milner <eddylynch9@gmail.com>
2022-03-23 20:21:43 +0000

    Updated sample microservice with new registration annotations

--- sample-microservice/src/main/java/ie/ul/microservices/sample/microservice/Application.java
+++ sample-microservice/src/main/java/ie/ul/microservices/sample/microservice/Application.java
@@ -1,11 +1,9 @@
 package ie.ul.microservices.sample.microservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import ie.ul.microservices.kernel.api.KernelMicroservice;
 
-@SpringBootApplication
-@ComponentScan(basePackages = "ie.ul.microservices.kernel.api")
+@KernelMicroservice
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
